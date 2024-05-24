c788cd94416d83664b83f0154de3906cd077a5ce
Michael Sommer <michael@sommer.gg>
2024-05-24 11:32:28 +0200

    Update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,7 +8,7 @@ import Skills from "@/components/skills";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4 pb-20">
+    <main className="flex flex-col items-center px-4 md:pb-20">
       <Intro />
       <SectionDivider />
       <About />
