2f3df387cb11b015b694cb9547dda5b45f1cc0a3
Michael Sommer <michael@sommer.gg>
2024-05-24 11:31:49 +0200

    Update about.tsx

--- client/components/about.tsx
+++ client/components/about.tsx
@@ -25,7 +25,7 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className="relative mb-28 w-full text-center leading-8 sm:mb-40 scroll-mt-28 flex flex-col justify-center items-center px-8 md:px-40"
+      className="relative mb-28 w-full text-center leading-8 sm:mb-40 scroll-mt-28 flex flex-col justify-center items-center md:px-40"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
