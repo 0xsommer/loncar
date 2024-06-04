00b2790d98df1b45cf42005f79f4cb922aacb0c4
Michael Sommer <michael@sommer.gg>
2024-06-04 16:41:18 +0200

    update testimonial

--- client/components/testimonial.jsx
+++ client/components/testimonial.jsx
@@ -2,7 +2,7 @@
 
 export default function Testimonial () {
   return (
-    <section className="py-8 bg-white shadow rounded-2xl overflow-hidden md:pb-20 md:pt-16 mb-16 md:mb-4 border border-black/5 dark:border-white/20 dark:text-white dark:bg-black/30 -mt-48 md:-mt-0">
+    <section className="py-8 bg-white shadow rounded-2xl overflow-hidden md:pb-16 md:pt-16 mb-16 md:mb-4 border border-black/5 dark:border-white/20 dark:text-white dark:bg-black/30 -mt-48 md:-mt-0">
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="relative">
           <img
@@ -13,26 +13,24 @@ export default function Testimonial () {
           <blockquote className="mt-8">
             <div className="max-w-3xl mx-auto text-center text-base leading-7 md:text-2xl md:leading-9 font-medium text-gray-900 dark:text-white px-2 md:px-16">
               <p>
-              &quot;Wir waren auf der Suche nach einem erfahrenen Partner, der uns in unserer digitalen Transformation unterstützen konnte. Sanda erwies sich als genau die Expertin, die wir brauchten. Wir sind heute besser positioniert, um den Herausforderungen einer digitalisierten Welt zu begegnen.&quot;
+              &quot;Sanda ist ein Digital Native der ersten Stunde und hat die digitale Transformation der Styria Media Group maßgeblich mitgestaltet.</p>
+              
+              <p className="mt-8">Nach dem Relaunch der Website von vecernji.hr list hat Sanda 24sata.hr zur klaren Nummer 1 unter den Nachrichtenportalen in Kroatien gemacht. Es war nur logisch, dass sie in ihrer führenden Rolle im Digitalteam der Kleinen Zeitung auch die Zahlen der Website und der App auf neue Rekordhöhen treiben konnte.&quot;
               </p>
             </div>
-            <footer className="mt-8">
+            <footer className="mt-10">
               <div className="md:flex md:items-center md:justify-center">
                 <div className="md:flex-shrink-0">
                   <img
-                    className="mx-auto h-10 w-10 rounded-full"
-                    src="/schweighofer.jpeg"
+                    className="mx-auto size-12 md:size-16 rounded-full"
+                    src="/bracun.jpeg"
                     alt=""
                   />
                 </div>
-                <div className="mt-4 text-center md:mt-0 md:ml-4 md:flex md:items-center">
-                  <div className="text-base font-medium text-gray-900 dark:text-white">Klaus Schweighofer</div>
+                <div className="max-w-sm text-center pt-4 md:pt-0 md:text-left md:ml-4 flex flex-col">
+                  <div className="text-base font-medium text-gray-900 dark:text-white">Helmut Bracun</div>
 
-                  <svg className="hidden md:block mx-1 h-5 w-5 text-black-600" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M11 0h3L9 20H6l5-20z" />
-                  </svg>
-
-                  <div className="text-base font-medium text-gray-500">Geschäftsführer, Styria Media International</div>
+                  <div className="text-sm font-medium text-gray-500 mt-2 md:mt-0">Former Head of Classifieds Styria Media Group, <br/>Ex-CEO 24sata & Vercernji List</div>
                 </div>
               </div>
             </footer>
