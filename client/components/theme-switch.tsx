18b4be630148b1d2b1ef4b0cd05dc8102acc978f
Michael Sommer <michael@sommer.gg>
2024-06-20 10:58:14 +0200

    Update theme-switch.tsx

--- client/components/theme-switch.tsx
+++ client/components/theme-switch.tsx
@@ -9,7 +9,7 @@ export default function ThemeSwitch() {
 
   return (
     <button
-      className="fixed top-4 md:top-6 right-4 md:right-8 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-gray-200 dark:border-gray-100/50 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 cursor-pointer z-10"
+      className="fixed top-4 md:top-6 right-4 md:right-8 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-gray-200 dark:border-gray-100/50 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 cursor-pointer z-50"
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
